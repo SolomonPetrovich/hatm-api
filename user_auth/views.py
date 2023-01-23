9dcdc5dde7bcd45d0850e31a68bf867f0b9715dd
Solomon Petrovich <mukhammedali.zholdasbay@gmail.com>
2023-01-23 20:29:02 +0600

    REMOVED USER_REGISTER VIEW AND SERIALIZER

--- user_auth/views.py
+++ user_auth/views.py
@@ -3,25 +3,6 @@ from .serializers import *
 from .models import CustomUser as User
 from .permissions import *
 
-class RegisterAPIView(generics.CreateAPIView):
-    serializer_class = UserRegistrationSerializer
-    permission_classes = (permissions.AllowAny,)
-    queryset = User.objects.all()
-
-    def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.save()
-        return response.Response(
-            {
-                "user": UserSerializer(
-                    user, context=self.get_serializer_context()
-                ).data,
-                "message": "User Created Successfully.  Now perform Login to get your token",
-            },
-            status=status.HTTP_201_CREATED,
-        )
-
 
 class UserRetrieveAPIView(generics.RetrieveAPIView):
     serializer_class = UserSerializer
