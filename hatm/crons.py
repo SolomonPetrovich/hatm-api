551b6cfd95112dfffb167cd5af3e2b9feebcd91b
Solomon Petrovich <mukhammedali.zholdasbay@gmail.com>
2023-02-16 23:44:47 +0600

    fixed cron job for cancel juz

--- hatm/crons.py
+++ hatm/crons.py
@@ -2,13 +2,12 @@ import datetime
 from .models import *
 
 def take_juz_from_user():
-    juzs = Juz.objects.filter(status='in Progress', deadline__date=datetime.date.today())
+    juzs = Juz.objects.filter(status='in Progress', deadline__lt=datetime.datetime.now())
     for juz in juzs:
-        if juz.deadline < datetime.now():
-            juz.status = 'free'
-            juz.user_id = None
-            juz.deadline = None
-            juz.save()
+        juz.status = 'free'
+        juz.user_id = None
+        juz.deadline = None
+        juz.save()
 
 
 def extend_the_deadline_of_hatm():
