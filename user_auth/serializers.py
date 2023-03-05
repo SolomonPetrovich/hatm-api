7b6bf15772af5592e953168a70b3c8014588a067
Solomon Petrovich <mukhammedali.zholdasbay@gmail.com>
2023-03-05 17:01:00 +0600

    added id to response users/me

--- user_auth/serializers.py
+++ user_auth/serializers.py
@@ -5,9 +5,10 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
     juz_set = JuzSerializer(many=True, read_only=True)
     email = serializers.ReadOnlyField()
 
     class Meta:
         model = User
-        fields = ('email', 'nickname', 'juz_set')
+        fields = ('id', 'email', 'nickname', 'juz_set')
