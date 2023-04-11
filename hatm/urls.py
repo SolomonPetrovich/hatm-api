281a0b2fd3d90327bdf5b0d927f086449ccedbd0
Solomon Petrovich <mukhammedali.zholdasbay@gmail.com>
2023-04-11 18:51:35 +0600

    changed in path from int to uuid

--- hatm/urls.py
+++ hatm/urls.py
@@ -3,11 +3,11 @@ from .views import *
 
 urlpatterns = [
     path('hatm/', HatmViewSet.as_view()),
-    path('hatm/<int:pk>/', HatmRetrieveView.as_view()),
-    path('hatm/<int:pk>/juzs/', JuzViewSet.as_view()),
+    path('hatm/<uuid:pk>/', HatmRetrieveView.as_view()),
+    path('hatm/<uuid:pk>/juzs/', JuzViewSet.as_view()),
     path('hatm/mine/', HatmMineViewSet.as_view()),
     path('juzs/mine/', JuzMineViewSet.as_view()),
     path('juzs/take/', JuzTakeView.as_view()),
-    path('juzs/cancel/<int:pk>/', JuzCancelView.as_view()),
-    path('juzs/finish/<int:pk>/', JuzFinishView.as_view()),
+    path('juzs/cancel/<uuid:pk>/', JuzCancelView.as_view()),
+    path('juzs/finish/<uuid:pk>/', JuzFinishView.as_view()),
 ]
